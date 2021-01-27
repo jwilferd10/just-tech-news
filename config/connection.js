a2a7c82ec1edf1f46570a217d4aea504df4bd122
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-01-26 17:03:52 -0800

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// The new Sequelize() function accepts the database name, MySQL username, and MySQL password (respectively) as parameters, 
-// Then we also pass configuration settings. Once we're done, we simply export the connection.
+let sequelize;
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-  
+}
+
 module.exports = sequelize;
