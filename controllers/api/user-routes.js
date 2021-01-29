598006b14cded1a28ff156e5bd21840a2f806933
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-01-28 17:46:45 -0800

    Feat: Added login and logout features, cookies also added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -25,6 +25,14 @@ router.get('/:id', (req, res) => {
         attributes: ['id', 'title', 'post_url', 'created_at']
       },
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
         model: Post,
         attributes: ['title'],
         through: Vote,
@@ -32,17 +40,17 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -52,18 +60,23 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
     });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // This route will be found at http://localhost:3001/api/users/login in the browser.
 router.post('/login', (req, res) => {
-  // The .findOne() Sequelize method looks for a user with the specified email. The result of the query is passed as dbUserData to the .then() part of the .findOne() method. 
-  // If the query result is successful (i.e., not empty), we can call .checkPassword(), which will be on the dbUserData object. 
-  // We'll need to pass the plaintext password, which is stored in req.body.password, into .checkPassword() as the argument.
   // expects {email: 'example@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
@@ -75,20 +88,32 @@ router.post('/login', (req, res) => {
       return;
     }
 
-    // res.json({ user: dbUserData });
-    // Note that the instance method was called on the user retrieved from the database, dbUserData. 
-    // Because the instance method returns a Boolean, we can use it in a conditional statement to verify whether the user has been verified or not.
-    // Verify the user
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
-      // If the match returns a false value, an error message is sent back to the client, and the return statement exits out of the function immediately.
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-    // if there is a match, the conditional statement block is ignored, and a response with the data and the message "You are now logged in." is sent instead.
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
 
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -102,7 +127,7 @@ router.put('/:id', (req, res) => {
     }
   })
     .then(dbUserData => {
-      if (!dbUserData[0]) {
+      if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
